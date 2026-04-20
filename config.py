8636a14ac53152a9895caf8f05eaee4edfa35b5b
pipalvarwal-hue <pipalvarwal@gmail.com>
2026-04-20 13:27:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8623381920")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "37241616"))
@@ -28,12 +28,12 @@ ADMINS = int(os.environ.get("ADMINS", "8452912208"))
 # Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
 # And Make Your Bot Admin In this channel with full rights.
 # if you don't want to upload in channel then leave it blank don't fill anything.
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "3773288403")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://ContentSave78:Contentsave78@cluster0.uowdgp1.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "contentcave78")
+DB_NAME = os.environ.get("DB_NAME", "@Hyjgtbot")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
 WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
